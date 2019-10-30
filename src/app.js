6f601f86c498bf34d9363fa3bad9106f064a6e50
kellyjoany <45659063+kellyjoany@users.noreply.github.com>
2019-10-30 16:53:25 -0300

    Meli-Post

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express()
+
+//rotas
+const index = require("./routes/index")
+const alunas = require("./routes/alunasRoute")
+const professoras = require("./routes/professorasRoute")
+
+app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
+
+app.use("/", index)
+app.use("/alunas", alunas)
+app.use("/professoras", professoras)
+
+module.exports = app
